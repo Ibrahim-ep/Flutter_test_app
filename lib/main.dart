396398e153c08527e5f013023fb0455fb4f5c5d6
Ibrahim Imad <ibrahim770476@gmail.com>
2026-08-08 13:50:10 +0300

    added const routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:testsss/constents/routes.dart';
 import 'package:testsss/firebase_options.dart';
 import 'package:testsss/views/login_view.dart';
 import 'package:testsss/views/register_view.dart';
@@ -15,9 +16,9 @@ void main() {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView(),
+        loginRoute: (context) => LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
       },
     ),
   );
@@ -81,7 +82,7 @@ class _NotesViesState extends State<NotesView> {
                       await FirebaseAuth.instance.signOut();
                       Navigator.of(
                         context,
-                      ).pushNamedAndRemoveUntil('/login/', (_) => false);
+                      ).pushNamedAndRemoveUntil(loginRoute, (_) => false);
                     }
                     break;
                   }
